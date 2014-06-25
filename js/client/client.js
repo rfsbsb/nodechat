052bfbdec7c23b242466fa780c4091eacf42dbdc
Rafael Ferreira Silva <rafael@rafaelsilva.net>
2014-06-25 09:22:52 -0300

    Adicionado a gravação no IndexdDB

--- js/client/client.js
+++ js/client/client.js
@@ -2,6 +2,7 @@ var ChatClient = function(personName) {
   this.ws = new WebSocket("ws://localhost:8080/");
   this.personName = personName;
   this.view = new ChatView();
+  this.db = new Database();
   var self = this;
 
   this.ws.onopen = function() {
@@ -13,6 +14,7 @@ var ChatClient = function(personName) {
     var data = JSON.parse(msg.data);
     if (data.proto == "send") {
       self.showMessage(data);
+      self.db.saveMessage(data.person);
     }
     if (data.proto == "list") {
       self.assebleListUsers(data);
@@ -39,3 +41,74 @@ var ChatClient = function(personName) {
     this.ws.send(message);
   }
 };
+
+var Database = function() {
+  window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+  window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+  window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+  if (!window.indexedDB) {
+      window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+  }
+
+  this.indexedDB = {};
+  this.db = null;
+  var self = this;
+
+  this.open = function () {
+    var req = window.indexedDB.open("nodechat", 1.0);
+
+    // hack para funcionar em versões antigas do Chrome
+    req.onsuccess = function(e) {
+      self.db = e.target.result;
+      var db = self.db;
+      if (db.setVersion) {
+        if (db.version != dbVersion) {
+          var req = db.setVersion(dbVersion);
+          req.onsuccess = function () {
+            if(db.objectStoreNames.contains("messages")) {
+              db.deleteObjectStore("messages");
+            }
+            var store = db.createObjectStore("messages", {keyPath: "timeStamp"});
+          };
+        }
+      }
+    }
+
+    req.onupgradeneeded = function(e) {
+      self.db = e.target.result;
+      var db = self.db;
+      if(db.objectStoreNames.contains("messages")) {
+        db.deleteObjectStore("messages");
+      }
+      var store = db.createObjectStore("messages", {keyPath: "timeStamp"});
+    }
+
+    req.onfailure = self.indexedDB.onerror;
+    req.onerror = function(e) {
+      console.error("Err:" + e);
+    }
+  }
+
+  this.saveMessage = function(item) {
+    if (!self.db) {
+      this.open();
+    }
+    var db = self.db;
+    var trans = db.transaction(['messages'], "readwrite");
+    var store = trans.objectStore("messages");
+
+    var data = {
+      "mensagem": item.message,
+      "pessoa": item.name,
+      "timeStamp": new Date().getTime()
+    };
+    var request = store.put(data);
+    request.onsuccess = function(e) {
+      console.log("Salvou");
+    };
+    request.onerror = function(e) {
+      console.error("Erro: ", e);
+    };
+  }
+
+}
